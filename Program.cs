4da4ef682feb57a30e034c2a72fe27a5b867bc66
kaiorod <caiorodrigo2775@gmail.com>
2021-08-07 17:41:28 -0300

    Adiciona versão inicial do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_word_pc1_2021_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Etec AB!");
+        }
+    }
+}
